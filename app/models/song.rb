61e517abfc87eda7cd1df8ad7576345cd46101ac
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-22 08:53:05 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,7 +5,7 @@ class Song < ApplicationRecord
     scope: %i[release_year artist_name],
     message: 'cannot be repeated by the same artist in the same year'
   }
-  validates :released, inclusion: { in: [true, false]}
+  validates :released, inclusion: { in: [true, false] }
   validates :artist_name, presence: true
 
 
